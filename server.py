1b91dffc94f570ca6b57ede1a98da6384b18e649
Port0r <marc.schroepfer@gmail.com>
2025-02-09 21:31:51 +0100

    a few changes to call F5 TTS

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import cld3
+
 from flask import Flask, request, send_file, jsonify
 import torch
 from transformers import pipeline
@@ -7,69 +7,22 @@ import json
 import base64
 app = Flask(__name__)
 
-def remember_language(lang: str):
-    with open('last-language', 'w') as f:
-        f.writelines([lang])
-
-def last_language() -> str | None:
-    if not os.path.exists('last-language'):
-        return None
-
-    with open('last-language', 'r') as f:
-        return f.readline()
-
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
-PIPE = pipeline(
-  "automatic-speech-recognition",
-  model="openai/whisper-medium",
-  chunk_length_s=30,
-  device=device,
-)
-
-@app.route('/v1/images/generations', methods=['POST'])
-def image():
-    data = request.get_json()  # Get the JSON input from the POST request
-    prompt = data['prompt']  # Extract the 'input' field
-        
-    os.system(f"./gen_image.sh \"{prompt}\"")
-    
-    file_text = open('image.png', 'rb')
-    file_read = file_text.read()
-    b64img = base64.encodebytes(file_read).decode('utf-8')
-    o = {"data": [{'b64_json': b64img}]}
-    return jsonify(o)
 
 @app.route('/v1/audio/speech', methods=['POST'])
 def speech():
     data = request.get_json()  # Get the JSON input from the POST request
-    text = data['input']  # Extract the 'input' field
-    prediction = cld3.get_language(text)
-    lang = prediction.language
-
-    if (prediction.is_reliable):
-        remember_language(lang)
-    else:
-        last_lang = last_language()
-        if not last_language == None:
-            lang = last_lang
-
+    text = data['input']  # Extract the 'input' field 
+    voice = data['voice']  # Extract the 'voice' field 
     
-    with open('textfile.txt', 'w') as f:  # Write the extracted text to a file
+    with open('../textfile.txt', 'w') as f:  # Write the extracted text to a file
+        text.replace(":", ".")
+        text.replace("!", ".")
+        text.replace("]:", "]")
         f.write(text)
         
-    os.system(f"./tts.sh {lang}")
+    os.system(f"./tts.sh en " + voice)
     
-    return send_file('output.wav', mimetype='audio/x-wav')  # Return the audio file in the HTTP response
-
-
-@app.route('/v1/audio/transcriptions', methods=['POST'])
-def transcribe():
-    file = request.files['file']
-    file.save('upload.wav')
-    transcription = PIPE('upload.wav')
-
-    return json.dumps(transcription), 200
+    return send_file('../output.wav', mimetype='audio/x-wav')  # Return the audio file in the HTTP response
 
 
 if __name__ == '__main__':
--- tts.sh
+++ tts.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-rm output.wav
+#SETUP
+F5_DIR="/home/porter/miniconda3/envs/f5-tts/"
+
+
 # Check if argument is provided
 if [ -z "$1" ]
 then
-    echo "No language supplied"
-    exit 1
+    echo "No language supplied."
+    LANGUAGE='en'
+fi
+
+# Check if argument is provided
+if [ -z "$2" ]
+then
+    echo "No voice name supplied."
+    VOICE='SexyScarlett'
+else
+    VOICE="$2"
 fi
 
 # Assign the argument to a variable based on your specifications
 echo "LANGUAGE $1"
+echo "VOICE $2"
 case $1 in
     'de')
         LANGUAGE='de-DE'
@@ -21,26 +34,11 @@ case $1 in
         LANGUAGE='it-IT'
         ;;
     *)
-        echo "Invalid language"
+        echo "Using en-US."
         LANGUAGE='en-US'
 	#exit;
         ;;
 esac
 
-# Check if file exists
-if [ ! -f "textfile.txt" ]; then
-    echo "File textfile.txt does not exist."
-    exit 1
-fi
 
-# Read the contents of the file into a variable
-CONTENTS_OF_TEXTFILE=$(cat textfile.txt)
-
-echo $CONTENTS_OF_TEXTFILE
-
-# Call pico2wave with the appropriate arguments
-pico2wave -l $LANGUAGE -w output_slow.wav "${CONTENTS_OF_TEXTFILE}"
-# Speed up things
-sox output_slow.wav output.wav tempo 1.2
-rm output_slow.wav
-rm textfile.txt
+TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1 python ../F5-TTS/src/f5_tts/infer/infer_cli.py -c "${F5_DIR}${VOICE}.toml" --load_vocoder_from_local --skip_audio_conversion
